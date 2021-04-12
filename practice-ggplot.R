647665f47a39e470fc488e95cabc7241555841c5
Rich <rich@posert.com>
2021-04-12 11:37:38 -0700

    Add penguin dataset

--- practice-ggplot.R
+++ practice-ggplot.R
@@ -1,6 +1,28 @@
 library(dplyr)
 library(readr)
 library(ggplot2)
+library(palmerpenguins)
+
+
+
+# Penguins ----------------------------------------------------------------
+
+# First, let's start with something fun to learn the basics
+
+View(penguins)
+
+## How would you make a histogram of all penguins' bill lengths?
+
+## How could you separate the histogram by species?
+
+## Suppose you wanted to compare each species bill shape (i.e., how their bill length and depth are related)?
+
+
+# Chromatography ----------------------------------------------------------
+
+# Next we can practice with something that is more relevant to our lab
 
 data <- read_csv('practice-data.csv', col_types = 'ddff') %>% 
   mutate(Channel = if_else(Channel == '2475ChA ex280/em350', 'Trp', 'GFP'))
+
+View(data)
